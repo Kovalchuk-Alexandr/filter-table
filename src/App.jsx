3e02b9500066601a4452aaee9f9fc9595f919a42
Kovalchuk-Alexandr <mo250568kaa@gmail.com>
2026-04-14 11:33:42 +0300

    final

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+
+function ProductCategoryRow({ category }) {
+	return (
+		<tr>
+			<th className='category-title' colSpan='2'>{category}</th>
+		</tr>
+	);
+}
+
+function ProductRow({ product }) {
+	const name = product.stocked ? product.name :
+		<span style={{ color: 'red' }}>{product.name}</span>
+
+	return (
+		<tr>
+			<td>{name}</td>
+			<td>{product.price}</td>
+		</tr>
+	);
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+	const rows = [];
+	let lastCategory = null;
+	// console.log('filterText: ', filterText);
+
+	products.forEach((product) => {
+		if (product.category.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+			return;
+		}
+		if (inStockOnly && !product.stocked) {
+			return;
+		}
+
+		if (product.category != lastCategory) {
+			rows.push(
+				<ProductCategoryRow category={product.category} key={product.category} />
+			);
+		}
+		rows.push(
+			<ProductRow product={product} key={product.name} />
+		);
+		lastCategory = product.category;
+	});
+
+	return (
+		<table className="frame product-table">
+			<thead>
+				<tr>
+					<th>Наименование</th>
+					<th>Цена</th>
+				</tr>
+			</thead>
+			<tbody>{rows}</tbody>
+		</table>
+	);
+}
+
+function SearchBar({filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange}) {
+	return (
+		<form className="frame">
+			<input
+				className="input-search"
+				type="text"
+				value={filterText}
+				placeholder="Search..."
+				onChange={(e) => onFilterTextChange(e.target.value)}
+			/>
+			<label>
+				<input type="checkbox" checked={inStockOnly} onChange={(e)=>onInStockOnlyChange(e.target.checked)}/> Показывать
+				только товар в наличии
+			</label>
+		</form>
+	);
+}
+
+function FilterableProductTable({ products }) {
+	const [filterText, setFilterText] = useState("");
+	const [inStockOnly, setInStockOnly] = useState(false);
+	return (
+		<div className="frame main-wrapper">
+			<SearchBar
+				filterTex={filterText}
+				inStockOnly={inStockOnly}
+				onFilterTextChange={setFilterText}
+				onInStockOnlyChange={setInStockOnly}
+			/>
+			<ProductTable
+				products={products}
+				filterText={filterText}
+				inStockOnly={inStockOnly}
+			/>
+		</div>
+	);
+}
+
+function App() {
+
+  return (
+    <>
+		<FilterableProductTable className="frame" products={PRODUCTS} />
+    </>
+  )
+}
+
+const PRODUCTS = [
+	{ category: "Фрукты", price: "$1", stocked: true, name: "Яблоко" },
+	{ category: "Фрукты", price: "$1", stocked: true, name: "Питахайя" },
+	{ category: "Фрукты", price: "$2", stocked: false, name: "Маракуйя" },
+	{ category: "Овощи", price: "$2", stocked: true, name: "Шпинат" },
+	{ category: "Овощи", price: "$4", stocked: false, name: "Тыква" },
+	{ category: "Овощи", price: "$1", stocked: true, name: "Горох" },
+];
+
+export default App
